1fa74f9515c55eeeca8763de502ae61c5919d6b8
xchenhao <chenhao_apple@163.com>
2019-09-16 13:27:25 +0800

    update topnav && footer

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -29,14 +29,14 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'BlogDemo 2',
+        'brandLabel' => '不可思议的博客',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
     $menuItems = [
-    		['label' => '文章管理', 'url' => ['/post/index']],
+            ['label' => '文章管理', 'url' => ['/article/index']],
     		['label' => '评论管理', 'url' => ['/comment/index']],
     		'<li><span class="badge">'.Comment::getPengdingCommentCount().'</span></li>',
     		['label' => '用户管理', 'url' => ['/user/index']],
@@ -69,14 +69,6 @@ AppAsset::register($this);
         <?= $content ?>
     </div>
 </div>
-
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
 
 <?php $this->endBody() ?>
 </body>
--- dev/sucai/referer.php
+++ dev/sucai/referer.php
@@ -1,3 +1,6 @@
 <meta name="referrer" content="never">
 
 <img class="detailImg" referrerpolicy="no-referrer" :src="url"/>
+
+https://www.cnblogs.com/wangtaolearning/p
+https://www.cnblogs.com/wangtaolearning/p/11294823.html
