de511a927bb7f47ae3e566b63b363154a763cf18
xchenhao <chenhao_apple@163.com>
2019-10-06 19:46:06 +0800

    add a tag nofollow

--- frontend/views/layouts/footer.php
+++ frontend/views/layouts/footer.php
@@ -13,23 +13,19 @@ use common\models\Links;
     width: 70%;
   }
   .about a {
-      white-space: nowrap;
-    /*background: pink;*/
+    white-space: nowrap;
   }
   .blank-box {
     margin-top: 200px;
   }
   footer {
-      position: fixed;
-      bottom: 0;
-      width: 100%;
-      z-index: 300;
-      /*transition: opacity 3s;*/
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    z-index: 300;
   }
   .item-hide {
-    /*display: none;*/
       opacity: 0;
-      /*height: 0;*/
   }
 </style>
 
@@ -37,9 +33,7 @@ use common\models\Links;
 <footer id="myfooter">
     <div class="about">
         <div class="links">
-            <?php
-            $links = Links::getLinks();
-            ?>
+            <?php $links = Links::getLinks(); ?>
             <a href="/" target="_blank" style="color: red">我的作品</a>
             <?php foreach ($links as $item): ?>
                 <a href="<?= $item['url'] ?>" target="_blank"><?= $item['name'] ?></a>
@@ -50,7 +44,7 @@ use common\models\Links;
     <div class="copyright">Copyright &copy; 2019-2020 不可思议的博客</div>
 
     <div style="width:300px;margin:0 auto; padding: 0;" class="copyright">
-        <a target="_blank" href="http://beian.miit.gov.cn/" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
+        <a target="_blank" href="http://beian.miit.gov.cn/" rel="nofollow" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
             <img src="/theme/news/img/ghs.png" style="float:left;"/>
             <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#FFFFFF;">闽ICP备17026538号-4
             </p>
